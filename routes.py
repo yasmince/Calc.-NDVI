eb2d0910f7f38f055868eb3e316da3bdffa9b012
Gabriel Sousa <gabriel05garcia@gmail.com>
2021-11-21 18:29:04 -0300

    teste

---
+++ routes.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+
+from main import insertUsuario
+
+app = Flask("API")
+
+@app.route("/olamundo", methods=["GET"])
+def olaMundo():
+    return {"planta": "Necessita de agua"}
+
+#@app.route("/cadastra/usuario", methods=["POST"])
+#def cadastraUsuario():
+
+ #   body = request.get_json()
+
+  #  usuario = insertUsuario(body["nome"], body["email"], body["senha"])
+
+   # return usuario
+if __name__ == "__main__":
+    app.run(debug=True)
+
